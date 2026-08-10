2c0d00b20df4e966e7610d4a84f1dacaf20873ec
Szymon Zych <szymonzych868@gmail.com>
2026-08-10 22:45:28 +0200

    add reverseArrayInPlace function to reverse an array in place

--- src/reverseArray/reverseArray.js
+++ src/reverseArray/reverseArray.js
@@ -5,3 +5,15 @@ function reverseArray(arr) {
 const original = [1, 2, 3];
 console.log(reverseArray(original));
 console.log(original);
+
+function reverseArrayInPlace(arr) {
+  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    const temp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp;
+  }
+}
+
+const originalInPlace = [1, 2, 3];
+reverseArrayInPlace(originalInPlace);
+console.log(originalInPlace);
