8bdf0ab49141f80025aebbb3cb3376d86385f531
Szymon Zych <szymonzych868@gmail.com>
2026-08-28 23:10:41 +0200

    add findMaxInPlace function to find the maximum value in an array without using Math.max

--- src/findMax/findMax.js
+++ src/findMax/findMax.js
@@ -3,3 +3,17 @@ function findMax(arr) {
 }
 
 console.log(findMax([1, 2, 3, 4, 5, 20, 24, 30]));
+
+function findMaxInPlace(arr) {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
+}
+
+console.log(findMaxInPlace([1, 2, 3, 4, 5, 20, 24, 30]));
