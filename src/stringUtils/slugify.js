d603ba4cc1cedee38060be76e39f352eaee13830
Szymon Zych <szymonzych868@gmail.com>
2026-09-03 23:06:32 +0200

    add slugify function to convert strings into URL-friendly format

---
+++ src/stringUtils/slugify.js
@@ -0,0 +1,10 @@
+function slugify(str) {
+  return str
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9\s-]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+}
+
+console.log(slugify("  Hello World! This is  JavaScript.  "));
