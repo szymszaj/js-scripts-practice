94d79d305d4d6bb28ce95e74b3c0814d8924a1fc
Szymon Zych <szymonzych868@gmail.com>
2026-09-15 22:13:23 +0200

    move partners data to separate module and import in merryFilter

--- src/merryFilter/merryFilter.js
+++ src/merryFilter/merryFilter.js
@@ -1,37 +1,4 @@
-const partners = [
-  {
-    id: 1,
-    name: "Anna",
-    age: 27,
-    city: "New York",
-    hobby: ["cooking", "traveling", "yoga"],
-    skills: ["cooking", "languages", "music"],
-  },
-  {
-    id: 2,
-    name: "Kate",
-    age: 31,
-    city: "Chicago",
-    hobby: ["sports", "reading"],
-    skills: ["sports", "programming"],
-  },
-  {
-    id: 3,
-    name: "Olivia",
-    age: 24,
-    city: "Los Angeles",
-    hobby: ["music", "photography"],
-    skills: ["music", "photography", "cooking"],
-  },
-  {
-    id: 4,
-    name: "Maggie",
-    age: 29,
-    city: "New York",
-    hobby: ["traveling", "sports"],
-    skills: ["languages", "sports"],
-  },
-];
+import { partners } from "./data.js";
 
 const filterPartners = (arr, filters = {}) => {
   return arr.filter((p) => {
